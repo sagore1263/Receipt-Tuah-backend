e3ce8ff0859a5fd906700aad4539de3ae3c07e55
Adin Lieblein <adinmlieblein@gmail.com>
2025-04-05 22:53:19 -0500

    quick date fix

--- js-backend/dateConvert.js
+++ js-backend/dateConvert.js
@@ -2,7 +2,7 @@ module.exports = (date, time) => {
     const [day, month, year] = date.split('/').map(Number);
     const [hour, minute] = time.split(':').map(Number);
 
-    const date = new Date(year, month - 1, day, hour, minute);
+    const unixdate = new Date(year, month - 1, day, hour, minute);
 
-    return Math.floor(date.getTime());
+    return Math.floor(unixdate.getTime());
 }
