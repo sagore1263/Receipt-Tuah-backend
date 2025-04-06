1f62e9db75db8c22eed55b6c097265d69e169319
Aditya YV <adityayv802@gmail.com>
2025-04-06 12:53:49 -0500

    latest

--- AI_API.py
+++ AI_API.py
@@ -105,7 +105,7 @@ async def generate_image_summary():
     image = Image.open('image.png')
 
     response = model.models.generate_content(
-        model="gemini-2.0-flash-thinking-exp-01-21",
+        model="gemini-2.0-flash",
         contents=["What is this image?", image])
     return response.text
 async def convert_image_to_text(path):
@@ -115,7 +115,7 @@ async def convert_image_to_text(path):
     image = Image.open(path)
 
     response = model.models.generate_content(
-        model="gemini-2.0-flash-thinking-exp-01-21",
+        model="gemini-2.0-flash",
         contents=["Convert this to text", image])
     return response.text
 dict_prompt = """Convert this receipt text into a dictionary. Use the following format - 
