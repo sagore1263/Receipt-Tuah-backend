39bf0d9045d2acb9b61cbd3efe7bf7ce6115052e
Adin Lieblein <adinmlieblein@gmail.com>
2025-04-06 00:52:34 -0500

    utility wipe

--- js-backend/utilityWipe.js
+++ js-backend/utilityWipe.js
@@ -3,4 +3,12 @@ const models = require('@m/mongo_models.js');
 
 (async () => {
     await dbConnect();
+
+    models.forEach(model => {
+        model.deleteMany({}).then(() => {
+            console.log(`All documents deleted from ${model.modelName} collection.`);
+        }).catch(err => {
+            console.error(`Error deleting documents from ${model.modelName} collection:`, err);
+        });
+    });
 })();
